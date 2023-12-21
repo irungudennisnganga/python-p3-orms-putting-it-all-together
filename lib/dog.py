0a28724feb8ce3df03ae27a1b8c3bf8c359e259e
+dennisngangairungu <+>
2023-12-21 20:07:50 +0300

    completed

--- lib/dog.py
+++ lib/dog.py
@@ -4,5 +4,91 @@ CONN = sqlite3.connect('lib/dogs.db')
 CURSOR = CONN.cursor()
 
 class Dog:
-    
-    pass
+    def __init__(self,name ,breed):
+        self.id = None
+        self.name = name
+        self.breed = breed
+        
+    @classmethod
+    def create_table(cls):
+        sql = """
+             CREATE TABLE IF NOT EXISTS dogs(
+                 id INTEGER PRIMARY KEY ,
+                 name TEXT,
+                 breed TEXT
+             )
+        """   
+        
+        CURSOR.execute(sql)
+        
+    @classmethod    
+    def drop_table(cls):
+        sql ="""
+            DROP TABLE IF EXISTS dogs
+        """   
+        CURSOR.execute(sql) 
+        
+        
+    def save(self):
+        sql ="""
+            INSERT INTO dogs(name,breed) 
+            VALUES(? , ?) 
+            
+        """  
+        CURSOR.execute(sql,(self.name,self.breed))
+        self.id=CURSOR.lastrowid  
+        
+    @classmethod    
+    def create(cls,name ,breed):
+        dog = cls(name,breed)
+        
+        dog.save()
+        return dog
+     
+    @classmethod     
+    def new_from_db(cls,row):
+        
+        
+        dog=cls(row[1] ,row[2])
+        dog.id=row[0]
+        return dog
+        
+    @classmethod    
+    def get_all(cls):
+        sql = """
+            SELECT *
+            FROM dogs
+        """
+        CURSOR.execute(sql)
+        rows = CURSOR.fetchall()
+        return [cls.new_from_db(row) for row in rows]
+    @classmethod
+    def find_by_name(cls, name):
+        sql="""
+            SELECT * FROM dogs
+            WHERE name = ?
+            LIMIT 1
+        """
+        
+        CURSOR.execute(sql , (name,))
+        row =CURSOR.fetchone()
+        return cls.new_from_db(row)
+
+
+    @classmethod
+    def find_by_id(cls, id):
+        sql="""
+            SELECT * FROM dogs
+            WHERE id = ?
+            LIMIT 1
+        """
+        
+        CURSOR.execute(sql , (id,))
+        row =CURSOR.fetchone()
+        return cls.new_from_db(row)
+
+    @classmethod
+    def find_or_create_by(cls,name , breed) :
+        passl
+            
+            
